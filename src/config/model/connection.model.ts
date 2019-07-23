59a96d4fff333164c04893a1d45d2a9ea42aaf83
Syforce <adrian.puiu@springtech.co>
2019-07-23 17:06:33 +0300

    typo

--- src/config/model/connection.model.ts
+++ src/config/model/connection.model.ts
@@ -1,10 +1,10 @@
 import { ConnectionType } from '../enum/connection-type.enum';
 
 export class Connection {
-	public url: string;
+	public database: string;
 	public host: string;
 	public type: ConnectionType;
 	public user?: string;
 	public password?: string;
-	public portn?: number;
+	public port?: number;
 }

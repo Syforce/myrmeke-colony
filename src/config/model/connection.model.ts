1e523e9bac1a3040ccf861270f09915c2215eabf
Syforce <adrian.puiu@springtech.co>
2019-07-23 16:53:55 +0300

    added user, port and password to database connection

--- src/config/model/connection.model.ts
+++ src/config/model/connection.model.ts
@@ -4,4 +4,7 @@ export class Connection {
 	public url: string;
 	public host: string;
 	public type: ConnectionType;
+	public user?: string;
+	public password?: string;
+	public portn?: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+// Abstract
 export { AbstractDatastore } from './abstract/datastore.abstract';
 export { IDatastore } from './abstract/datastore.interface';
 
+// Config Model
 export { Database } from './config/model/database.model';
 export { Connection } from './config/model/connection.model';
 
+// Config Schema
 export { SchemaConfig } from './config/schema/schema-config.model';
 export { SchemaFieldConfig } from './config/schema/schema-field-config.model';
 
+// Config Enum
 export { ConnectionType } from './config/enum/connection-type.enum';
 export { SchemaFieldType } from './config/enum/schema-field-type.enum';
 
+// Service
 export { ISchemaService } from './service/abstract/schema-service.interface';
 export { datastoreService, DatastoreService } from './service/datastore.service';
 export { DynamicConnection } from './service/dynamic-connection.component';
